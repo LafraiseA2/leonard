b3fa507fcafa493071cfa0cab5c918c68079331c
Leonard <leonardtnh2003@gmail.com>
2021-08-15 22:13:49 +0700

    Bai moi

--- thunghiem.cpp
+++ thunghiem.cpp
@@ -1,29 +1,86 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <Windows.h>
+#include <ctime>
 #define max 100
+// Bài 378: Tìm 1 chữ số xuất hiện nhiều nhất
 using namespace std;
-/* Bài 250: Hảy cho biết các phần tử trong mảng có lập thành cấp số cộng hay không? Nếu có chỉ ra công sai d*/
 
-void nhap(int a[], int &n)      // Khai bao nhap mang
+void nhap(int a[][max], int &dong, int &cot)
 {
     do
     {
-        cout <<"Nhap n: "; cin >> n;
-        if(n < 0 || n > max)
-            cout << "Nhap lai n.\n ";
-    } while (n < 0 || n > max);
-    
-    for(int i = 1; i <= n; i++)     //Nhap cac bien trong mang
+        cout <<"Nhap so dong: "; cin >> dong;
+        if(dong < 0 || dong > max)
+            cout <<"Nhap lai dong.\n";
+    } while (dong < 0 || dong > max);
+    do
     {
-        cout <<"a[" << i << "]: ";
-        cin >> a[i];
+        cout <<"Nhap so cot: "; cin >> cot;
+        if(cot < 0 || cot > max)
+            cout <<"Nhap lai cot.\n";
+    } while (cot < 0 || cot > max);
+    for(int i = 0; i < dong; i++)
+    {
+        for(int j = 0; j < cot; j++)
+        {
+            cout <<"a[" << i <<"][" << j <<"]: ";
+            cin >> a[i][j];
+        }
+    }    
+}
+
+void xuat(int a[][max], int dong, int cot)
+{
+    for(int i = 0; i < dong; i++)
+    {
+        for(int j = 0; j < cot; j++)
+        {
+            cout << a[i][j] <<"     ";
+        }
+        cout <<"\n";
     }
 }
 
+void tachso(int a, int b[])
+{
+    int chuso;
+    a = abs(a);
+    do
+    {
+        chuso = a % 10;
+        b[chuso]++;
+        a /= 10;
+    } while (a > 0);
+    
+}
+
+int nhieunhat(int a[][max], int dong, int cot)
+{
+    int b[10] = {0};
+    for(int i = 0; i < dong; i++)
+    {
+        for(int j = 0; j < cot; j++)
+        {
+            tachso(a[i][j], b);
+        }
+    }
+    int chusonhieunhat = 0;
+    for(int i = 0; i < 10; i++)
+    {
+        if(b[chusonhieunhat] < b[i])
+            chusonhieunhat = i;
+    }
+
+    return chusonhieunhat;
+}
 int main()
 {
-    int a[][max];
-    int n;
-    nhap(a,n);
+    int a[max][max];
+    int dong, cot;
+    nhap(a,dong,cot);
+    xuat(a,dong,cot);
+    cout <<"Chu so xuat hien nhieu nhat la: " << nhieunhat(a, dong,cot);
+    return 0;
 }
